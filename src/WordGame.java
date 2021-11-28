0ce7bad566b8841ad085a962db2bdef7f6c89d60
PRESSO <caff1nepill@gmail.com>
2021-11-28 17:33:34 +0900

    Add ScorePanel

--- src/WordGame.java
+++ src/WordGame.java
@@ -1,4 +1,7 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,7 +11,8 @@ public class WordGame extends JFrame {
     Vector<Word> wordVector = new Vector<>();
     private GamePanel panel = new GamePanel();
     private UserCharacter user = new UserCharacter();
-    JTextField jtf = new JTextField();
+    private JTextField jtf = new JTextField();
+    private GameData gameData = new GameData();
 
     public WordGame () {
         setTitle("WordGame");
@@ -16,20 +20,25 @@ public class WordGame extends JFrame {
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container c =  getContentPane();
+
         c.add(panel, BorderLayout.CENTER);
 
         jtf.setSize(800, 30);
         jtf.addActionListener(new TextInputListener());
         c.add(jtf, BorderLayout.SOUTH);
 
-        wordVector.add(new Word("Hello", (int) (Math.random() * 800), (int) (Math.random() * 100)));
-        wordVector.add(new Word("World", (int) (Math.random() * 800), (int) (Math.random() * 100)));
-        wordVector.add(new Word("Java", (int) (Math.random() * 800), (int) (Math.random() * 100)));
-        wordVector.add(new Word("C++", (int) (Math.random() * 800), (int) (Math.random() * 100)));
-        wordVector.add(new Word("Python", (int) (Math.random() * 800), (int) (Math.random() * 100)));
-        wordVector.add(new Word("Javascript", (int) (Math.random() * 800), (int) (Math.random() * 100)));
-        wordVector.add(new Word("OOP", (int) (Math.random() * 800), (int) (Math.random() * 100)));
-        wordVector.add(new Word("PP", (int) (Math.random() * 800), (int) (Math.random() * 100)));
+        ScorePanel sp = new ScorePanel();
+        sp.setPreferredSize(new Dimension(200, 500));
+        c.add(sp, BorderLayout.EAST);
+
+        wordVector.add(new Word("Hello", (int) (Math.random() * 600), (int) (Math.random() * 100)));
+        wordVector.add(new Word("World", (int) (Math.random() * 600), (int) (Math.random() * 100)));
+        wordVector.add(new Word("Java", (int) (Math.random() * 600), (int) (Math.random() * 100)));
+        wordVector.add(new Word("C++", (int) (Math.random() * 600), (int) (Math.random() * 100)));
+        wordVector.add(new Word("Python", (int) (Math.random() * 600), (int) (Math.random() * 100)));
+        wordVector.add(new Word("Javascript", (int) (Math.random() * 600), (int) (Math.random() * 100)));
+        wordVector.add(new Word("OOP", (int) (Math.random() * 600), (int) (Math.random() * 100)));
+        wordVector.add(new Word("PP", (int) (Math.random() * 600), (int) (Math.random() * 100)));
 
         WordMoveThread wmt = new WordMoveThread();
         wmt.start();
@@ -45,6 +54,34 @@ public class WordGame extends JFrame {
                 item.draw(g);
             }
             repaint();
+        }
+    }
+
+    class ScorePanel extends JPanel {
+        public ScorePanel() {
+            setBackground(Color.GRAY);
+            setLayout(new FlowLayout());
+
+            Border marginStage = new EmptyBorder(100, 15, 20, 0);
+            JLabel stageLabel = new JLabel("STAGE " + gameData.stage);
+            stageLabel.setFont(new Font("Noto Sans", Font.BOLD, 20));
+            stageLabel.setBorder(marginStage);
+            add(stageLabel);
+
+            Border marginName = new EmptyBorder(0, 15, 10, 0);
+            Border marginNumber = new EmptyBorder(0, 30, 10, 15);
+            for (Score item : gameData.score) {
+                JLabel scoreNameLabel = new JLabel(item.scoreName), scoreNumberLabel = new JLabel(Integer.toString(item.scoreNumber));
+
+                scoreNameLabel.setFont(new Font("Noto Sans", Font.BOLD, 15));
+                scoreNumberLabel.setFont(new Font("Noto Sans", Font.BOLD, 15));
+
+                scoreNameLabel.setBorder(marginName);
+                scoreNumberLabel.setBorder(marginNumber);
+
+                add(scoreNameLabel);
+                add(scoreNumberLabel);
+            }
         }
     }
 
@@ -122,7 +159,7 @@ class Word {
     public void draw(Graphics g) {
         if (item.equals("None")) g.setColor(Color.BLACK);
         else if (item.equals("Life")) g.setColor(Color.GREEN);
-        else if (item.equals("Slow")) g.setColor(Color.BLUE);
+        else if (item.equals("Slow")) g.setColor(Color.CYAN);
         g.drawString(word, x, y);
     }
 }
@@ -132,7 +169,7 @@ class UserCharacter {
     int width, height;
     int life;
     public UserCharacter() {
-        this.x = 400;
+        this.x = 300;
         this.y = 350;
         this.width = 30;
         this.height = 30;
@@ -140,12 +177,33 @@ class UserCharacter {
     }
 
     public void draw(Graphics g) {
-        g.setColor(Color.WHITE);
+        g.setColor(Color.BLACK);
         g.fillOval(x, y, width, height);
 
         g.setColor(Color.RED);
         for (int i = 0; i < life; i++) {
             g.fillOval(x + (i < (life / 2) ? -1 * 20 * i : 20 * i) - 13, y + 50, 15, 15);
         }
+    }
+}
+
+class GameData {
+    int stage;
+    Vector<Score> score = new Vector<>();
+    public GameData() {
+        stage = 1;
+        score.add(new Score("Total Score"));
+        score.add(new Score("Word Score"));
+        score.add(new Score("Life Score"));
+        score.add(new Score("Stage Bonus"));
+
+    }
+}
+class Score {
+    String scoreName;
+    int scoreNumber;
+    public Score(String scoreName) {
+        this.scoreName = scoreName;
+        this.scoreNumber = 0;
     }
 }
